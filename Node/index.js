12ba993036b773f61f13af8d605b1ed5558010cf
boodii0308 <boodii.tb@gmail.com>
2021-01-27 17:12:31 -0800

    jan/27

---
+++ Node/index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes");
+var supervillian = require("supervillains");
+var mySV = supervillian.random();
+var mySH = superheroes.random();
+
+//console.log(mySH);
+console.log(mySV);
